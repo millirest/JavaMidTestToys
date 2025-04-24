580f4f986101001188f2d46b7a1e7cef4fbccfc6
millirest <millirest@mail.ru>
2025-04-24 17:17:20 +0500

    add Toy class

---
+++ src/Toy.java
@@ -0,0 +1,48 @@
+public class Toy {
+    private int id; // 
+    private String name; // Имя
+    // private int quantity; // Колличество
+    private double weight; // Вес
+
+    public Toy(int id, String name,  double weight) { // int quantity,
+        this.id = id;
+        this.name = name;
+        // this.quantity = quantity;
+        this.weight = weight;
+    }
+    
+    //Gets
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    // public int getQuantity() {
+    //     return quantity;
+    // }
+
+    public double getWeight() {
+        return weight;
+    }
+
+
+    //Sets
+    public void setId(int id) {
+        this.id = id;
+    }
+    public void setname(String name) {
+        this.name = name;
+    }
+
+    // public void setQuantity(int quantity) {
+    //     this.quantity = quantity;
+    // }
+
+    public void setWeight(double weight) {
+        this.weight = weight;
+    }
+
+}
