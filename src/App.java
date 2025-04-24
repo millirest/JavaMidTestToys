dca1c3771318dd04e238ef3175ba2fe2af4c92ad
millirest <millirest@mail.ru>
2025-04-24 18:01:36 +0500

    out in txt file

--- src/App.java
+++ src/App.java
@@ -1,3 +1,5 @@
+import java.io.File;
+import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
@@ -12,11 +14,13 @@ public class App {
         double [] weight = new double[3];
         int [] ids = new int[3];
         String [] names = new String[3];
-        
+        toys.add(new toy(0, "name1", 1));
+        toys.add(new toy(1, "name2", 0.5));
+        toys.add(new toy(2, "name2", 1));
+        // for (int i=1; i < 5; i++){
+        //     toys.add(new toy(i, "name"+i, 4+i));
+        // }
 
-        for (int i=0; i < 4; i++){
-            toys.add(new toy(i, "name"+i, 4+i));
-        }
         System.out.println(toys);
 
         for (int i =0; i < 3; i++){
@@ -31,5 +35,16 @@ public class App {
             queue.offer(toys.get(i));
         }
         System.out.println(toys);
+
+        try (PrintWriter writer = new PrintWriter(new File("output.txt"))) {
+            for (int i = 0; i < 10; i++){
+                toy theToy = queue.poll();
+                writer.println(theToy.getId()+" "+theToy.getName()+" "+ theToy.getWeight());
+                queue.offer(theToy);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
+
 }
