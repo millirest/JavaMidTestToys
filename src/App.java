42d0d4fb502b48f4c2822bb8bd676909e3849183
millirest <millirest@mail.ru>
2025-04-24 17:51:24 +0500

    create Queue comparator

--- src/App.java
+++ src/App.java
@@ -1,5 +1,8 @@
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
+import java.util.PriorityQueue;
+import java.util.Queue;
 
 public class App {
     public static void main(String[] args) throws Exception {
@@ -14,13 +17,19 @@ public class App {
         for (int i=0; i < 4; i++){
             toys.add(new toy(i, "name"+i, 4+i));
         }
-        
+        System.out.println(toys);
+
         for (int i =0; i < 3; i++){
             toy theToy = toys.get(i);
             ids[i] = theToy.getId();
             names[i] =theToy.getName();
             weight[i] = theToy.getWeight();
-    }
+        }
+        Queue<toy> queue = new PriorityQueue<>(3, Comparator.comparingDouble(toy::getWeight));
 
+        for (int i = 0; i < 3; i++){
+            queue.offer(toys.get(i));
+        }
+        System.out.println(toys);
     }
 }
